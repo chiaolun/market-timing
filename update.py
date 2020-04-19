045b62deee25ba6979ab897c14550e19743fad7d
Chiao Cheng <chiao@alum.mit.edu>
2020-04-19 12:04:29 -0700

    refactoring and added alpha computation

--- update.py
+++ update.py
@@ -9,6 +9,10 @@ for params in [
             output='MA200 fast and slow - untradeable version.ipynb',
             kwargs=dict(symbol='^GSPC'),
         ),
+        dict(
+            output='MA200 fast and slow - untradeable version - all_history.ipynb',
+            kwargs=dict(symbol='^GSPC', start_date=None),
+        ),
 ]:
     pm.execute_notebook(
         'MA200 fast and slow - template.ipynb',
