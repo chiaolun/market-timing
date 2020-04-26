d0da6807b83dbfefe668d9383a0ed815bc01cb4b
Chiao Cheng <chiao@alum.mit.edu>
2020-04-25 17:30:07 -0700

    added leverage

--- update.py
+++ update.py
@@ -6,12 +6,36 @@ for params in [
             kwargs=dict(symbol='SPY'),
         ),
         dict(
+            output='MA200 fast and slow - tradeable version - 1.1x.ipynb',
+            kwargs=dict(symbol='SPY', leverage=1.1),
+        ),
+        dict(
+            output='MA200 fast and slow - tradeable version - 1.2x.ipynb',
+            kwargs=dict(symbol='SPY', leverage=1.2),
+        ),
+        dict(
             output='MA200 fast and slow - untradeable version.ipynb',
             kwargs=dict(symbol='^GSPC'),
         ),
         dict(
+            output='MA200 fast and slow - untradeable version - 1.1x.ipynb',
+            kwargs=dict(symbol='^GSPC', leverage=1.1),
+        ),
+        dict(
+            output='MA200 fast and slow - untradeable version - 1.2x.ipynb',
+            kwargs=dict(symbol='^GSPC', leverage=1.2),
+        ),
+        dict(
             output='MA200 fast and slow - untradeable version - all_history.ipynb',
             kwargs=dict(symbol='^GSPC', start_date=None),
+        ),
+        dict(
+            output='MA200 fast and slow - untradeable version - 1.1x - all_history.ipynb',
+            kwargs=dict(symbol='^GSPC', leverage=1.1, start_date=None),
+        ),
+        dict(
+            output='MA200 fast and slow - untradeable version - 1.2x - all_history.ipynb',
+            kwargs=dict(symbol='^GSPC', leverage=1.2, start_date=None),
         ),
 ]:
     pm.execute_notebook(
